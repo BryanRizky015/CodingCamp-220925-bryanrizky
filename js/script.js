fe6c8e925e609ffecc4a7b7c5403ae25199887a6
BryanRizky015 <148904308+BryanRizky015@users.noreply.github.com>
2025-09-25 22:02:53 +0700

    add mini project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+function WelcomeMessage() {
+  let username = prompt("Enter your Name: ");
+
+  if (username) {
+    document.getElementById("welcome").textContent = `Hi ${username}, Welcome!`;
+    document.getElementById("subtitle").textContent = `Welcome to my portfolio, ${username}`;
+    alert(`Welcome to my portfolio, ${username}`);
+  } else {
+    document.getElementById("welcome").textContent = "Hi Visitor, Welcome!";
+    alert("Welcome To My Portfolio");
+  }
+}
+window.onload = WelcomeMessage;
+
+
+// ===============================
+// Validasi Form
+// ===============================
+const form = document.getElementById("messageForm");
+const result = document.getElementById("formResult");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const userName = document.getElementById("name").value;
+  const userMessage = document.getElementById("message").value;
+  console.log("Name:", userName);
+  console.log("Message:", userMessage);
+   alert(`Thank you, ${userName}! Your message: "${userMessage}" has been received.`);
+  form.reset();
+});
